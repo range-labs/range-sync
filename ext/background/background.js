58133539b2b8bdcb69f2f8ce598f93399b8afaad
Joseph Miller <joseph@range.co>
2020-08-26 15:55:56 -0700

    Add Asana, GitHub, Gmail, Lever, and Metabase integrations (#4)

--- ext/background/background.js
+++ ext/background/background.js
@@ -1,8 +1,13 @@
 'use strict';
 
-chrome.tabs.onUpdated.addListener((_tabId, _info, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, _info, tab) => {
   // no-op unless done loading
   if (!tab.status || !tab.status.localeCompare('complete') == 0) return;
+
+  // no-op if title is in the global block list
+  for (const re of BLOCK_LIST.title) {
+    if (re.test(tab.title)) return;
+  }
 
   // no-op unless we have enough info to make an attachment
   if (!tab.url || !tab.title) return;
@@ -15,11 +20,14 @@ chrome.tabs.onUpdated.addListener((_tabId, _info, tab) => {
     .then((sessions) =>
       Promise.all(
         sessions.map((s) => {
+          const a = attachment(s, tab);
+          if (!a) return Promise.resolve();
+
           return recordInteraction(
             {
               interaction_type: 'VIEWED',
-              idempotency_key: tab.title,
-              attachment: attachment(s, tab),
+              idempotency_key: `${tabId}::${tab.title}`,
+              attachment: a,
             },
             authorize(s)
           );
@@ -32,47 +40,71 @@ chrome.tabs.onUpdated.addListener((_tabId, _info, tab) => {
 function attachment(session, tab) {
   const url = new URL(tab.url);
 
+  // To avoid spam, do not automatically track domains without paths
+  if (url.pathname.length <= 1) return null;
+
+  const provider = providerInfo(url, tab.title);
+  if (!provider) return null;
+
   return {
-    ...providerInfo(url),
-    name: tab.title,
-    html_url: url.hostname + url.pathname,
+    ...provider,
+    html_url: url.href,
     org_id: session.org.org_id,
     type: 'LINK',
     subtype: 'NONE',
   };
 }
 
-function providerInfo(url) {
-  const base = url.hostname + url.pathname;
+function providerInfo(url, title) {
+  // For consistency, remove trailing forward slashes
+  const base = url.hostname + url.pathname.replace(/\/+$/, '');
   // Loop through the known providers and check the url against them
-  for (const reConfig of REGEX) {
+  for (const filter of FILTER) {
     let sourceId = '';
-    for (const reUrl of reConfig.url_regex) {
-      if (reUrl.test(base)) {
-        const pageMatch = base.match(reConfig.page_regex);
-        if (!!pageMatch) {
-          sourceId = pageMatch[0];
-          break;
+    for (const reUrl of filter.url_regex) {
+      if (!reUrl.test(base)) continue;
+      if (blocked(filter.block_list, url, title)) return null;
+
+      for (const processor of filter.processors) {
+        sourceId = processor.source_id_processor(url);
+        if (!!sourceId) {
+          return {
+            name: processor.title_processor(title),
+            provider: filter.provider,
+            provider_name: filter.provider_name,
+            source_id: sourceId,
+          };
         }
       }
     }
-    // If no match, try the next one
-    if (!sourceId) continue;
-
-    return {
-      provider: reConfig.provider,
-      provider_name: reConfig.provider_name,
-      source_id: sourceId,
-    };
   }
 
   // If there's no known provider, generate one based on the URL
   const hostParts = url.hostname.split('.');
   return {
+    name: title,
     source_id: base,
     // i.e. 'subdomain.nytimes.com' -> 'chromeext_nytimes'
     provider: `chromeext_${hostParts[hostParts.length - 2]}`,
     // i.e. 'subdomain.nytimes.com' -> 'nytimes.com (via Range Sync)'
-    provider_name: `${hostParts[hostParts.length - 2]}.${hostParts[hostParts.length - 1]} (via Range Sync)`,
+    provider_name: `${hostParts[hostParts.length - 2]}.${
+      hostParts[hostParts.length - 1]
+    } (via Range Sync)`,
   };
+}
+
+function blocked(blockList, url, title) {
+  if (!blockList) return false;
+
+  if (!!blockList.title) {
+    for (const s of blockList.title) {
+      if (s.test(title)) return true;
+    }
+  }
+
+  if (!!blockList.url) {
+    for (const r of blockList.url) {
+      if (r.test(url.href)) return true;
+    }
+  }
 }
--- ext/background/regex.js
+++ ext/background/regex.js
@@ -1,18 +1,186 @@
 'use strict';
 
-const REGEX = [
-  // Stack Overflow User
+const GUID_REGEX =
+  '({){0,1}[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(}){0,1}';
+
+const BLOCK_LIST = {
+  title: [/^chrome:\/\//],
+};
+
+const FILTER = [
+  // Asana
   {
-    url_regex: [/.*stackoverflow\.com\/.*/, /.*stackenterprise\.co\/.*/],
-    page_regex: /users\/([\d\w]+)/,
-    provider: 'stackoverflow',
-    provider_name: 'Stack Overflow',
+    url_regex: [/app\.asana\.com/],
+    provider: 'asana',
+    provider_name: 'Asana',
+    processors: [
+      // Ticket
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /(?<=0\/[\d]+\/)([\d]+)/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'Customers - Planning - Draft slides - Asana' -> 'Draft slides'
+        title_processor: (t) => t.split(' - ')[2].trim(),
+      },
+    ],
+    block_list: [/board$/],
   },
-  // Stack Overflow Question
+  // Stack Overflow
   {
-    url_regex: [/.*stackoverflow\.com\/.*/, /.*stackenterprise\.co\/.*/],
-    page_regex: /questions\/([\d]+)/,
+    url_regex: [/stackoverflow\.com/, /stackenterprise\.co/],
     provider: 'stackoverflow',
     provider_name: 'Stack Overflow',
+    processors: [
+      // User
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /users\/([\d\w]+)/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'username - Stack Overflow' -> 'username'
+        title_processor: (t) => t.split(' - ')[0].trim(),
+      },
+      // Question
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /questions\/([\d]+)/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'html - my questions - Stack Overflow' -> 'my questions'
+        title_processor: (t) => t.split(' - ')[1].trim(),
+      },
+    ],
+  },
+  // GitHub
+  {
+    url_regex: [/github\.com\/.+/],
+    provider: 'github',
+    provider_name: 'GitHub',
+    processors: [
+      // Issue
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /([a-z-\/]+)\/issues\/[0-9]+/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'I have an issue · Issue #1234 · range-labs/mono' -> 'I have an issue (Issue #1234)'
+        title_processor: (t) => {
+          const p = t.split(' · ');
+          return `${p[0].trim()} (${p[1].trim()})`;
+        },
+      },
+      // Pull Request
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /([a-z-\/]+)\/pull\/[0-9]+/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'I have a PR · Pull Request #1234 · range-labs/mono' -> 'I have a PR (PR #1234)'
+        title_processor: (t) => {
+          const p = t.split(' · ');
+          return `${p[0].trim()} (PR #${p[1].split('#')[1]})`;
+        },
+      },
+    ],
+    block_list: {
+      url: [/\/pulls/],
+    },
+  },
+  // Gmail
+  {
+    url_regex: [/mail\.google\.com/],
+    provider: 'gmail',
+    provider_name: 'Gmail',
+    processors: [
+      // Email
+      {
+        source_id_processor: (url) => {
+          const hashRegex = /[a-zA-z]+$/;
+          return reMatch(url.hash, hashRegex, 0);
+        },
+        // i.e. 'Fwd: Action required: Email title - me@domain.com - My Email Account' -> 'Fwd: Action required: Email title'
+        title_processor: (t) => t.split(' - ')[0],
+      },
+    ],
+    block_list: {
+      title: [
+        /^Inbox/,
+        /^Sent Mail/,
+        /^Starred/,
+        /^Snoozed/,
+        /^Drafts/,
+        /^Important/,
+        /^\(no subject\)/,
+      ],
+      url: [/inbox$/, /sent$/, /starred$/, /snoozed$/, /drafts$/, /important$/],
+    },
+  },
+  // Lever
+  {
+    url_regex: [/hire\.lever\.co\/.+/],
+    provider: 'lever',
+    provider_name: 'Lever',
+    processors: [
+      // Candidate
+      {
+        source_id_processor: (url) => {
+          const pathRegex = new RegExp(`candidates\\/${GUID_REGEX}`);
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'Full Name - Lever Hire' -> 'Full Name'
+        title_processor: (t) => `${t.split('–')[0].trim()} (Candidate)`,
+      },
+      // Interview
+      {
+        source_id_processor: (url) => {
+          const pathRegex = new RegExp(`interviews\\/${GUID_REGEX}`);
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'Full Name - Lever Hire' -> 'Full Name'
+        title_processor: (t) => `${t.split('–')[0].trim()} (Interview)`,
+      },
+    ],
+    block_list: {
+      title: [/Lever Hire/],
+      url: [/\/candidates\?/],
+    },
+  },
+  // Metabase
+  {
+    url_regex: [/metabase\..*/],
+    provider: 'metabase',
+    provider_name: 'Metabase',
+    processors: [
+      // Question
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /question\/[0-9]+/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'How many users so we have? · Metabase' -> 'How many users so we have?'
+        title_processor: (t) => t.split('·')[0].trim(),
+      },
+      // Dashboard
+      {
+        source_id_processor: (url) => {
+          const pathRegex = /dashboard\/[0-9]+/;
+          return reMatch(url.pathname, pathRegex, 0);
+        },
+        // i.e. 'General Metrics · Dashboard · Metabase' -> 'General Metrics'
+        title_processor: (t) => t.split('·')[0].trim(),
+      },
+    ],
+    block_list: {
+      title: [/^Question/, /^Metabase/],
+      // Metabase collections just have the title "Metabase" which is kinda useless
+      // for our purposes
+      url: [/\/collection\/[0-9]+$/],
+    },
   },
 ];
+
+function reMatch(str, re, index) {
+  const match = str.match(re);
+  return match ? match[index] : null;
+}
