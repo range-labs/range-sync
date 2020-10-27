64cc5c0907b399a0efdfedb78fa9cf9121ff10f0
Joseph Miller <joseph.a.miller101@gmail.com>
2020-10-27 13:39:05 -0700

    Dropbox now works with both URL schemes (#28)

--- ext/background/filter/dropbox_paper.js
+++ ext/background/filter/dropbox_paper.js
@@ -1,7 +1,7 @@
 'use strict';
 
 registerFilter({
-  url_regex: [/dropbox\.com\/.*\.paper/],
+  url_regex: [/dropbox\.com\/.*\.paper/, /paper\.dropbox\.com/],
   provider: 'dropbox_paper',
   provider_name: (_url) => 'Dropbox Paper',
   type: (_url) => 'DOCUMENT',
@@ -10,10 +10,12 @@ registerFilter({
     // Document
     {
       source_id_processor: (url) => {
-        const rePath = /scl\/fi\/[a-z0-9]+/;
-        return reMatch(url.pathname, rePath, 0);
+        const rePath1 = /scl\/fi\/[a-z0-9]+/;
+        const rePath2 = /\/doc\/.*--(.*$)/;
+        const match1 = reMatch(url.pathname, rePath1, 0);
+        return !!match1 ? match1 : reMatch(url.pathname, rePath2, 1);
       },
-      title_processor: (t) => t,
+      title_processor: (t) => trimLastPart(t, ' – '),
     },
   ],
 });
