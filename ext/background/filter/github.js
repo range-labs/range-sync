abf4e3f261cec218c42ad3ef08692dc947718f31
Joseph Miller <joseph.a.miller101@gmail.com>
2020-11-09 11:54:39 -0800

    Update the GitHub attachments and source ID to match the backend (#40)

--- ext/background/filter/github.js
+++ ext/background/filter/github.js
@@ -10,27 +10,29 @@ registerFilter({
   processors: [
     // Issue
     {
+      // i.e. 'https://github.com/range-labs/mono/issues/8166' -> 'range-labs/mono#8166'
       source_id_processor: (url) => {
-        const rePath = /([a-z-\/]+)\/issues\/[0-9]+/;
-        return reMatch(url.pathname, rePath, 0);
+        const rePath = /\.com\/(.+)\/(.+)\/issues\/([0-9]+)/;
+        const match = url.href.match(rePath);
+        return match ? `${match[1]}/${match[2]}#${match[3]}` : null;
       },
       // i.e. 'I have an issue · Issue #1234 · range-labs/mono' -> 'Issue #1234: I have an issue'
       title_processor: (t) => {
-        const p = t.split(' · ');
-        return `${p[1].trim()}: ${p[0].trim()}`;
+        return t.split(' · ')[0].trim();
       },
     },
     // Pull Request
     {
+      // i.e. 'https://github.com/range-labs/mono/pull/8166' -> 'range-labs/mono#8166'
       source_id_processor: (url) => {
-        const rePath = /([a-z-\/]+)\/pull\/[0-9]+/;
-        return reMatch(url.pathname, rePath, 0);
+        const rePath = /\.com\/(.+)\/(.+)\/pull\/([0-9]+)/;
+        const match = url.href.match(rePath);
+        return match ? `${match[1]}/${match[2]}#${match[3]}` : null;
       },
-      // i.e. 'I have a PR by jmiller101· Pull Request #1234 · range-labs/mono' -> 'PR #1234: I have a PR'
+      // i.e. 'I have a PR by jmiller101 · Pull Request #1234 · range-labs/mono' -> 'I have a PR'
       title_processor: (t) => {
         const p = t.split(' · ');
-        const title = p[0].split(' by ')[0].trim();
-        return `PR #${p[1].split('#')[1]}: ${title}`;
+        return p[0].split(' by ')[0].trim();
       },
     },
   ],
